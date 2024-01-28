8818e7be6d43544d9e7a339ebca20a8b52fec686
James Viney <jamesviney04@gmail.com>
2024-01-28 19:40:24 +0000

    added brain block and hopefully made parchment work

--- src/main/java/com/plite/testmod/TestMod.java
+++ src/main/java/com/plite/testmod/TestMod.java
@@ -1,6 +1,7 @@
 package com.plite.testmod;
 
 import com.mojang.logging.LogUtils;
+import com.plite.testmod.block.ModBlocks;
 import com.plite.testmod.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -22,6 +23,7 @@ public class TestMod {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
--- src/main/java/com/plite/testmod/item/ModCreativeModeTab.java
+++ src/main/java/com/plite/testmod/item/ModCreativeModeTab.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTab {
-    public static final CreativeModeTab TEST_TAB = new CreativeModeTab("testtab") {
+    public static final CreativeModeTab TEST_TAB = new CreativeModeTab("test_tab") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.SCRAP.get());
